c6060d18d2df228047f0f3331600b7c1b1cd1737
SaeedShariati <doloresdotnet@gmail.com>
2022-10-07 11:05:40 +0330

    added the product model

---
+++ src/app/shared/product.model.ts
@@ -0,0 +1,6 @@
+export class Product{
+  constructor(public Id:string,public name:string
+    ,public description:string,public price:Number,
+    public departments:string[],public imageUrl:string
+    ){}
+}

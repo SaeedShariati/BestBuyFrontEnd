998d0df5cec1f7889c5d0d6aac04424c133cdac2
SaeedShariati <doloresdotnet@gmail.com>
2022-10-07 10:46:40 +0330

    added product-list and product-item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ContentComponent } from './content/content.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavigationComponent,
-    ContentComponent
+    ContentComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
